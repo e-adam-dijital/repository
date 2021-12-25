4a820dff32a3a73c9017efeb07bc262522b67f0f
e-adam-dijital <e-adam-dijital@protonmail.com>
2021-12-25 14:29:34 +0300

    tema seçimi güncelleme 11

---
+++ faprika/dksstore-script_yonetimi-anasayfa.js
@@ -0,0 +1,7 @@
+// Anasayfa
+  $(document).ready(function(){
+	 $("#divIcerik").before('<div class="divIcerikContainer"> </div>');
+     $("#divIcerik").appendTo(".divIcerikContainer");
+})
+
+$('.ozelAlan5').remove();
