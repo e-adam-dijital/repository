fa41bd0dcfdd020d1d0820d1304b06ba96d78523
e-adam-dijital <e-adam-dijital@protonmail.com>
2021-12-29 17:16:43 +0300

    tema seçimi güncelleme 18

--- ticimax/denizbutik/hulyakeser-script_yonetimi-header_footer.js
+++ ticimax/denizbutik/hulyakeser-script_yonetimi-header_footer.js
@@ -1,7 +1,7 @@
 // Tüm Sayfalar - Header 
 
   $(document).ready(function(){
-  $(".mobilMenu #HeaderMenu2 > li:first-child").before(`<li><a href="/" target="_self" title="anasayfa"><img src="/Uploads/EditorUploads/anasayfa.jpg" alt="mobilMenu"></a></li>`)
+  //$(".mobilMenu #HeaderMenu2 > li:first-child").before(`<li><a href="/" target="_self" title="anasayfa"><img src="/Uploads/EditorUploads/anasayfa.jpg" alt="mobilMenu"></a></li>`)
 	 $(".mobilMenu #HeaderMenu2 > li > a").each(function(){
   var kategoriAd = $(this).text();
   console.log(kategoriAd)
