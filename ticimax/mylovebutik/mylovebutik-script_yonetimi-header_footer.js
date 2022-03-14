5c79f57ab7390de54bbab735222f8a7cf2ad838a
e-adam-dijital <e-adam-dijital@protonmail.com>
2022-03-14 13:07:40 +0300

    tema seçimi güncelleme 49

--- ticimax/mylovebutik/mylovebutik-script_yonetimi-header_footer.js
+++ ticimax/mylovebutik/mylovebutik-script_yonetimi-header_footer.js
@@ -12,30 +12,16 @@
  
 //  Tüm Sayfalar 
 
-<script>
   var IndirimOrani = 0.70;
   var sepettekiFiyat = "-%30";
   var sepettekiFiyatEn="30% Discount In Cart";
-</script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
-<script>
-    WebFont.load({
+// <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+
+  WebFont.load({
   custom: {
             families: ['spartan, sans-serif'],
             urls: ['https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap'],
             timeout: 2000,
         }
-    });
-
-</script>
-
-<style>
-#ETBIS {float: left;width: 100%;text-align: center;}
-#ETBIS > div {max-width: 100px;display: inline-block;margin-bottom: 20px;}
-@media (min-width: 767px) {
-  .FooterBottom {padding-bottom: 0!important;}
-}
-</style>
-
-
+  });
